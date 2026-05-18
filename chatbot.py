f26ab7583449b454ef54703665909c0755d220cf
pradeep981b-ops <pradeep981b@gmail.com>
2026-05-18 21:47:22 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+# Basic Chatbot - CodeAlpha Internship
+
+def chatbot():
+
+    print("===== BASIC CHATBOT =====")
+    print("Type 'bye' to exit")
+    print()
+
+    while True:
+
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Bot: Hi!")
+
+        elif user == "how are you":
+            print("Bot: I'm fine, thanks!")
+
+        elif user == "what is your name":
+            print("Bot: I am a simple Python chatbot.")
+
+        elif user == "who created you":
+            print("Bot: I was created using Python.")
+
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+
+        else:
+            print("Bot: Sorry, I don't understand that.")
+
+# Run chatbot
+chatbot()
